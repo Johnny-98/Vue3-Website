6542765593ab91b29b3d1d57155024135b527347
Peter <petiodjonov@yahoo.com>
2023-11-18 13:24:56 +0200

    Install vuecli and bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import BootstrapVue from 'bootstrap-vue';
+import { createApp } from 'vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App)
-
+// Vue.use(BootstrapVue);
 app.use(router)
 
 app.mount('#app')
